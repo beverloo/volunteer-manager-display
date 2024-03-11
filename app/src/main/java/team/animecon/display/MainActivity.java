cad8e25033390f63ae1371ff62262fc13c251818
Peter Beverloo <pbeverloo@gmail.com>
2024-03-11 20:37:12 +0000

    Initial import
    This is the default Android Studio C++ template, we'll prune from here

---
+++ app/src/main/java/team/animecon/display/MainActivity.java
@@ -0,0 +1,36 @@
+package team.animecon.display;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import team.animecon.display.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'display' library on application startup.
+    static {
+        System.loadLibrary("display");
+    }
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Example of a call to a native method
+        TextView tv = binding.sampleText;
+        tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'display' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
