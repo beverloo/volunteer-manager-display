a95869187801ca9ea0a2441ca4685c0bef2b2cbe
Peter Beverloo <pbeverloo@gmail.com>
2024-03-12 10:06:33 +0000

    Enable JavaScript in the WebView, capture navigations

--- app/src/main/java/team/animecon/display/MainActivity.java
+++ app/src/main/java/team/animecon/display/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.util.Log;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 import team.animecon.display.databinding.ActivityMainBinding;
@@ -36,6 +39,18 @@ public class MainActivity extends AppCompatActivity {
 
         mBrightnessController.initialise();
         mKioskController.initialise();
+
+        // Initialise WebView:
+        {
+            WebView webView = binding.webview;
+            WebSettings webSettings = webView.getSettings();
+            webSettings.setJavaScriptEnabled(true);
+
+            webView.setWebViewClient(new WebViewClient()
+            {
+                // no behaviour necessary (yet)
+            });
+        }
 
         // BrightnessController test:
         {
